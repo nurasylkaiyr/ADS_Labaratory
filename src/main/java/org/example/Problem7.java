257728722c269a48bc2e8aa2b51aa04113885647
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-01 22:08:24 +0600

    Problem-7

---
+++ src/main/java/org/example/Problem7.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.Scanner;
+public class Problem7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        reverse(arr, 0, n-1);
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void reverse(int[] arr, int a, int b){
+        if(a >= b){
+            return;
+        }
+        int c = arr[a];
+        arr[a] = arr[b];
+        arr[b] = c;
+        reverse(arr, a+1, b-1);
+
+    }
+}
