7c3a50fcac41222cd571b56e535e58d7b756fed4
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-01 20:33:12 +0600

    Problem-2

---
+++ src/main/java/Problem3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Problem3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if(checkPrime(n, 2)){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Composite");
+        }
+    }
+    public static boolean checkPrime(int n, int i){
+        if(n <= 2){
+            return (n == 2) ? true : false;
+        }
+        if(n % i == 0){
+            return false;
+        }
+        if(i * i > n) {
+            return true;
+        }
+        return checkPrime(n, i+1);
+    }
+}
+
+
+
+
+
+
+
