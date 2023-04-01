60753d3657065b2c6adbf3153b06beb0cc912ee3
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-01 17:26:23 +0600

    Problem-2

---
+++ src/main/java/org/example/Problem2.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+public class Problem2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        double avg = findAvg(n, arr);
+        System.out.println(avg);
+    }
+    public static double findAvg(int n, int[] arr){
+        if(n == 0){
+            return 0;
+        }
+        else{
+            double sum = findAvg(n-1, arr)*(n-1);
+            sum += arr[n-1];
+            return sum/n;
+        }
+    }
+}
