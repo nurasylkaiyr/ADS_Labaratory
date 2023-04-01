88df27badd486d143f8c7e25ff8db40d939775e0
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-04-01 20:55:25 +0600

    Problem-5

---
+++ src/main/java/org/example/Problem5.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.util.Scanner;
+public class Problem5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(seqFibonacci(n));
+    }
+    public static int seqFibonacci(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        return seqFibonacci(n-1)+seqFibonacci(n-2);
+    }
+}
